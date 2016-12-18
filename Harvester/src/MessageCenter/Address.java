44e416c950b968030cec3dd93f950125d91ad465
teague <teague.bick@gmail.com>
2016-12-18 13:34:55 -0500

    Setting up TCP connection handling and addressing

---
+++ Harvester/src/MessageCenter/Address.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MessageCenter;
+
+/**
+ *
+ * @author teague
+ */
+public class Address {
+    private String address;
+    
+    public Address(String addr){
+        address = addr;
+    }
+}
