9f6b424254238ee78d4dc45af96c278cf38e9709
teague <teague.bick@gmail.com>
2016-12-19 00:05:51 -0500

    Setting up TCP connection handling and addressing

--- Harvester/src/MessageCenter/tcp/TcpConnectionReader.java
+++ Harvester/src/MessageCenter/tcp/TcpConnectionReader.java
@@ -5,6 +5,9 @@
  */
 package MessageCenter.tcp;
 
+import MessageCenter.Message;
+import MessageCenter.MessageCenter;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -20,9 +23,29 @@ public class TcpConnectionReader implements Runnable {
     
     @Override
     public void run() {
+        HashMap<String, String> messageData = new HashMap<>();
+        
+        
         while(input.hasNext()){
-            System.out.println("Read: " + input.nextLine());
+            String next = input.nextLine();
+            if(next.contains(":")){
+                String[] kv = next.split(" *: *");
+                messageData.put(kv[0], kv[1]);
+            }
         }
+        
+        if(messageData.containsKey("Destination") && 
+                messageData.containsKey("Source") &&
+                messageData.containsKey("Payload")){
+            Message m = new Message(
+                messageData.get("Destination"),
+                messageData.get("Source"),
+                messageData.get("Payload"));
+                    
+            MessageCenter.getMessageCenter().receiveMessage(m);
+        }
+        
+        input.close();
     }
     
 }
