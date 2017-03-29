8792aec4b4142d1dd56d8fd4595358d4f340d092
teague <teague.bick@gmail.com>
2017-03-28 21:49:38 -0400

    * Changing constructors for machine, updater, and gateway to static methods * Passing machine to gateway and updater separately

--- src/main/java/com/tabgok/heimdallr/HeimdallrServer.java
+++ src/main/java/com/tabgok/heimdallr/HeimdallrServer.java
@@ -1,20 +1,24 @@
 package com.tabgok.heimdallr;
 
-import com.tabgok.entity.factory.MachineFactory;
+import com.tabgok.entity.updater.MachineUpdater;
+import com.tabgok.entity.Machine;
 import com.tabgok.server.Gateway;
 
 /**
- * Starts up an instance of the Heimdallr system, which consists of two parts:
- *  1. Start a machine factory, which gathers all the information on the system and constructs an in-memory view of the local "Machine".
- *  2. Start up a "Gateway", which is an HttpServer, in order to respond to queries and provide JSON data
+ * Starts up a HiemdallrServer, which gathers information on the local machine 
+ * and makes it available via an HttpServer.
  */
 
 public class HeimdallrServer {
 
     public static void main(String args[]){
-        // Create an Http Server, with access to an in-memory model of the local "Machine"
-        Gateway g = new Gateway(MachineFactory.getMachine());
-        //Start running commands and building out the "Machine"
-        MachineFactory.startUpdater();
+        Machine machine = Machine.getMachine();
+        MachineUpdater updater = MachineUpdater.getUpdater(machine);
+        Gateway gateway = Gateway.getGateway(machine);
+        
+        //Start the job scheduler to update the Machine model
+        updater.start();
+        //Start the Http Gateway
+        gateway.start();
     }
 }
